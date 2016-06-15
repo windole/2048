06b46757351ac97bfe388d2edef5401103ac89be
windole <1322670109@qq.com>
2016-06-15 22:07:41 +0800

    random

--- js/main2048.js
+++ js/main2048.js
@@ -9,6 +9,8 @@ function newgame() {
 	//初始化棋盘格
 	init();
 	//在随机两个格子生成数字
+	generateOneNumber();
+	generateOneNumber();
 }
 
 function init(){
@@ -20,4 +22,69 @@ function init(){
             gridCell.css('left', getPosLeft( i , j ) );
         }
     }
+
+    for (var i = 0; i < 4; i++) {
+    	board[i]=new Array();
+    	for(var j = 0;j<4;j++)
+    		board[i][j] = 0;
+    	 }
+
+    	 updateBoardView();
 }
+
+
+function updateBoardView(){
+
+    $(".number-cell").remove();
+    for( var i = 0 ; i < 4 ; i ++ )
+        for( var j = 0 ; j < 4 ; j ++ ){
+            $("#grid-container").append( '<div class="number-cell" id="number-cell-'+i+'-'+j+'"></div>' );
+            var theNumberCell = $('#number-cell-'+i+'-'+j);
+
+            if( board[i][j] == 0 ){
+                theNumberCell.css('width','0px');
+                theNumberCell.css('height','0px');
+                theNumberCell.css('top',getPosTop(i,j) + 45 );
+                theNumberCell.css('left',getPosLeft(i,j) + 45 );
+            }
+            else{
+                theNumberCell.css('width','90px');
+                theNumberCell.css('height','90px');
+                theNumberCell.css('top',getPosTop(i,j));
+                theNumberCell.css('left',getPosLeft(i,j));
+                theNumberCell.css('background-color',getNumberBackgroundColor( board[i][j] ) );
+                theNumberCell.css('color',getNumberColor( board[i][j] ) );
+                theNumberCell.text( board[i][j] );
+            }
+        }
+}
+
+function generateOneNumber() {
+	if (nospace(board)) 
+		return false;
+	
+
+	//随机一个位置
+	var randx = parseInt(Math.floor(Math.random()*4));
+	var randy = parseInt(Math.floor(Math.random()*4));
+	while(true){
+		if (board[randx][randy]==0) 
+			break;
+		
+
+		randx = parseInt(Math.floor(Math.random()*4));
+		randy = parseInt(Math.floor(Math.random()*4));
+	}
+
+	//随机一个数字
+
+	var randNumber =Math.random() < 0.65 ? 2 : 4;
+
+	//在随机位置显示随机数字
+	board[randx][randy] = randNumber;
+	showNumberWithAnimation(randx,randy,randNumber);
+		return true;
+	
+}
+
+
