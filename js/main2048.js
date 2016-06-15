58a535034c0c5c24d7f8b043c6c01e1a5ae85c92
windole <1322670109@qq.com>
2016-06-15 14:41:35 +0800

    初始页面

---
+++ js/main2048.js
@@ -0,0 +1,23 @@
+var board = new Array();
+var score = 0;
+
+$(document).ready(function () {
+	newgame();
+});
+
+function newgame() {
+	//初始化棋盘格
+	init();
+	//在随机两个格子生成数字
+}
+
+function init(){
+    for( var i = 0 ; i < 4 ; i ++ ){
+        for( var j = 0 ; j < 4 ; j ++ ){
+
+            var gridCell = $('#grid-cell-'+i+"-"+j);
+            gridCell.css('top', getPosTop( i , j ) );
+            gridCell.css('left', getPosLeft( i , j ) );
+        }
+    }
+}
---
+++ js/support2048.js
@@ -0,0 +1,7 @@
+function getPosTop(i,j) {
+	return 10+i*100;
+}
+
+function getPosLeft(i,j) {
+	return 10+j*100;
+}
